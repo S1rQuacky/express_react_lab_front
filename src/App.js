98d83c5e1e2f9f18ffaef6fe33f6b5546a8594c1
Neide Urbina <neide.n.urbina@gmail.com>
2021-10-30 16:20:57 -0500

    fixing frontend

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+
+//Import components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Route, Switch } from "react-router-dom";
+
+//Import pages
+import Home from './components/Header';
+import About from './pages/About';
+import Projects from './pages/Projects';
+
+function App() {
+  const URL = "https://exre-lab.herokuapp.com/"
+
+  return (
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/projects">
+          <Projects URL={URL} />
+        </Route>
+        <About path="/about">
+          <About URL={URL} />
+        </About>
+      </Switch>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
