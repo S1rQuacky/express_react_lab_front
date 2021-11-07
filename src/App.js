ae9548522894b83a21e6e5cf2ac8f9e93454cb99
Neide Urbina <neide.n.urbina@gmail.com>
2021-11-06 22:40:59 -0500

    some css completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-
 //Import components
 import Header from './components/Header';
 import Footer from './components/Footer';
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 function Home(props) {
-    return <h1>Home</h1>;
+    return <h1 className="home">Web designs by Neide</h1>;
 }
 
 export default Home;
